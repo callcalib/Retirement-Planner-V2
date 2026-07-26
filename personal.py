72ff463fb0fa4d15d4bd8086be2f7dfe88038782
callcalib <cal@aiinak.com>
2026-07-26 14:00:00 +0100

    Add personal financial data structure

---
+++ personal.py
@@ -0,0 +1,22 @@
+PERSONAL = {
+
+    "retirement_age": 55,
+
+    "assets": {
+        "sipp": 300000,
+        "isa": 298217,
+        "nsi": 62693,
+        "uk_house": 65000,
+    },
+
+    "property": {
+        "irish_house_1": 220000,
+        "irish_house_2": 60000,
+    },
+
+    "income": {
+        "irish_rent": 10000,
+        "state_pension_age": 67,
+    },
+
+}
