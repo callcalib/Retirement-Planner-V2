28067dfe0d7a6a3efbda0fa920c6070b63939bcf
callcalib <cal@aiinak.com>
2026-07-26 14:05:22 +0100

    Create countries.py with economic data
    Added a dictionary of countries with various economic indicators and costs of living.

---
+++ data/countries.py
@@ -0,0 +1,77 @@
+COUNTRIES = {
+
+"Batumi":{
+
+    "house_price":170000,
+
+    "property_tax":150,
+
+     "healthcare":100,
+
+    "pension_tax":0,
+
+    "foriegn_interest_tax":0,
+
+    "local_interest_tax":5%,
+
+    "irish_rental_tax":0,
+
+    "bread":0.85,
+
+    "milk":1.45,
+
+    "rice":1.60,
+
+    "chicken":4.20,
+
+    "beef":8.90,
+
+    "eggs":2.20,
+
+    "electricity":0.10,
+
+    "petrol":1.06,
+
+    "internet":12,
+
+    "mobile":6,
+
+    "restaurant_meal":8,
+
+    "coffee 1KG":2.5
+
+},
+
+"Albania":{
+
+...
+
+},
+
+"Varna":{
+
+"Baguio":{
+
+...
+
+},
+
+"North Macedonia — Ohrid":{
+
+...
+
+}
+
+}
+
+country = {
+# 🇬🇪 Georgia — Batumi/Kobuleti
+#🇵🇭 Philippines — Baguio
+#🇧🇬 Bulgaria — Sandanski
+#🇲🇰 North Macedonia — Ohrid
+#🇦🇱 Albania
+#🇨🇾 Cyprus mountain
+#🇲🇾 Malaysia
+#🇹🇭 Thailand
+#🇪🇸 Spain
+#🇬🇧 UK comparison
